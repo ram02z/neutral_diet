96473f25efe29f2ae4ff5a04c39fce43f165c832
Omar Zeghouani <omarzeghouanii@gmail.com>
2022-11-13 21:22:19 +0000

    init

---
+++ ui/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Neutral Diet</h1>
+      <div>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
