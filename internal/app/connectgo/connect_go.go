d8f9aa6f267574a7eaa9db37648233b96aa30f50
Omar Zeghouani <omarzeghouanii@gmail.com>
2022-12-15 18:16:17 +0000

    chore: add message about static files

--- internal/app/connectgo/connect_go.go
+++ internal/app/connectgo/connect_go.go
@@ -109,7 +109,8 @@ func NewConnectGoServer(
 
 func (s *Server) start(logger *zerolog.Logger) {
 	go func() {
-		logger.Info().Str("address", s.Server.Addr).Msg("Listening for connect-go")
+		logger.Info().Str("address", s.Server.Addr).Str("path", "/").Msg("Serving static files")
+		logger.Info().Str("address", s.Server.Addr).Str("path", "/api").Msg("Listening for connect-go")
 		s.notify <- s.Server.ListenAndServe()
 		close(s.notify)
 	}()
