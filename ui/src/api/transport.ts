fcc39acf66821562ee8c85b1aae3b51986ba7df8
Omar Zeghouani <omarzeghouanii@gmail.com>
2023-01-18 23:20:14 +0000

    run prettier

--- ui/src/api/transport.ts
+++ ui/src/api/transport.ts
@@ -4,6 +4,6 @@ const transport = createConnectTransport({
   baseUrl: `http://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}/api`,
 });
 
-export const ID_TOKEN_HEADER = "X-ID-Token";
+export const ID_TOKEN_HEADER = 'X-ID-Token';
 
 export default transport;
