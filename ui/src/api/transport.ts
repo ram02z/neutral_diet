8c25b2e83d3929d38da3fd1f2153c7e58cf80e7e
Omar Zeghouani <omarzeghouanii@gmail.com>
2023-01-18 11:34:13 +0000

    Replace BASE_URL env

--- ui/src/api/transport.ts
+++ ui/src/api/transport.ts
@@ -1,7 +1,7 @@
 import { createConnectTransport } from '@bufbuild/connect-web';
 
 const transport = createConnectTransport({
-  baseUrl: import.meta.env.VITE_BACKEND_BASE_URL,
+  baseUrl: `http://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}/api`,
 });
 
 export default transport;
