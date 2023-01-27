8c41ffcc0df8a913af07840d145ad0e35b25ea65
Omar Zeghouani <omarzeghouanii@gmail.com>
2023-01-27 13:56:26 +0000

    use notistack

--- ui/src/Root.tsx
+++ ui/src/Root.tsx
@@ -4,7 +4,9 @@ import { HelmetProvider } from 'react-helmet-async';
 import { RecoilRoot } from 'recoil';
 
 import { ConfirmProvider } from 'material-ui-confirm';
+import { SnackbarProvider } from 'notistack';
 
+import { notifications } from '@/config';
 import ThemeProvider from '@/theme/Provider';
 
 const container = document.getElementById('root') as HTMLElement;
@@ -17,7 +19,12 @@ function render(App: ComponentType) {
         <HelmetProvider>
           <ThemeProvider>
             <ConfirmProvider>
-              <App />
+              <SnackbarProvider
+                maxSnack={notifications.maxSnack}
+                anchorOrigin={notifications.options.anchorOrigin}
+              >
+                <App />
+              </SnackbarProvider>
             </ConfirmProvider>
           </ThemeProvider>
         </HelmetProvider>
