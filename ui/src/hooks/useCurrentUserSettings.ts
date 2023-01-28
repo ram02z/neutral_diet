9466ee5b8845282ca3cc614e0942a96abae710c2
Omar Zeghouani <omarzeghouanii@gmail.com>
2023-01-28 16:36:03 +0000

    delete old hook

--- ui/src/hooks/useCurrentUserSettings.ts
+++
@@ -1,8 +0,0 @@
-import { useRecoilValueLoadable } from 'recoil';
-
-import { RemoteUserSettingsState } from '@/store/user';
-
-export function useCurrentUserSettings() {
-  const value = useRecoilValueLoadable(RemoteUserSettingsState);
-  return value.state === 'loading' ? undefined : value.valueOrThrow();
-}
