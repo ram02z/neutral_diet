937a30e9f651643d406bd09e88d4a7a63469128e
Omar Zeghouani <omarzeghouanii@gmail.com>
2023-02-04 19:16:37 +0000

    update searchbar and fooditemcard

--- ui/src/components/FoodItemCard/FoodItemCard.tsx
+++ ui/src/components/FoodItemCard/FoodItemCard.tsx
@@ -1,4 +1,5 @@
-import { Card, CardActionArea, CardContent } from '@mui/material';
+import { Add } from '@mui/icons-material';
+import { Card, CardActionArea, CardContent, IconButton, Typography } from '@mui/material';
 
 import { AggregateFoodItem } from '@/api/gen/neutral_diet/food/v1/food_item_pb';
 
@@ -8,9 +9,19 @@ type FoodItemCardProps = {
 
 function FoodItemCard({ foodItem }: FoodItemCardProps) {
   return (
-    <Card sx={{ minWidth: 300 }}>
+    <Card sx={{ minWidth: 250 }}>
       <CardActionArea>
-        <CardContent>{foodItem.foodName}</CardContent>
+        <CardContent>
+          <IconButton sx={{ float: 'right' }} aria-label="add">
+            <Add />
+          </IconButton>
+          <Typography sx={{ textTransform: 'capitalize' }} variant="h5" component="div">
+            {foodItem.foodName.toLowerCase()}
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            {foodItem.medianCarbonFootprint}
+          </Typography>
+        </CardContent>
       </CardActionArea>
     </Card>
   );
--- ui/src/pages/Search/Search.tsx
+++ ui/src/pages/Search/Search.tsx
@@ -1,27 +1,32 @@
+import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 
-import { SearchRounded } from '@mui/icons-material';
-import { InputAdornment, TextField } from '@mui/material';
+import { Close, SearchRounded } from '@mui/icons-material';
+import { FormControl, IconButton, InputAdornment, OutlinedInput } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 import { Box } from '@mui/system';
 
+import { AggregateFoodItem } from '@/api/gen/neutral_diet/food/v1/food_item_pb';
 import FoodItemCard from '@/components/FoodItemCard';
 import { FoodItemsState } from '@/store/food';
-import { useState } from 'react';
-import { AggregateFoodItem } from '@/api/gen/neutral_diet/food/v1/food_item_pb';
 
 function Search() {
-  const foodItems = useRecoilValue(FoodItemsState)
+  const foodItems = useRecoilValue(FoodItemsState);
   const [searchFoodItems, setSearchFoodItems] = useState<AggregateFoodItem[]>([]);
-  const [search, setSearch] = useState('')
+  const [searchText, setSearchText] = useState('');
 
   const handleSubmit = (e) => {
     if (e?.key === 'Enter') {
       const newFoodItems = foodItems.filter((foodItem) => {
-        return foodItem.foodName.toLowerCase().includes(search.toLowerCase())
-      })
-      setSearchFoodItems(newFoodItems)
+        return foodItem.foodName.toLowerCase().includes(searchText.toLowerCase());
+      });
+      setSearchFoodItems(newFoodItems);
     }
+  };
+
+  const clearSearch = () => {
+    setSearchText('');
+    setSearchFoodItems([]);
   };
 
   // TODO: use react-window
@@ -36,24 +41,32 @@ function Search() {
         pt="4vh"
         disableEqualOverflow
       >
-        <Grid>
-          <TextField
-            variant="outlined"
-            placeholder="Search for food"
-            value={search}
-            onKeyDown={handleSubmit}
-            onChange={(e) => setSearch(e.target.value as string)}
-            InputProps={{
-              startAdornment: (
+        <Grid xs={8} sm={7} md={6} lg={5} xl={4}>
+          <FormControl fullWidth variant="outlined">
+            <OutlinedInput
+              placeholder="Search for food"
+              value={searchText}
+              onKeyDown={handleSubmit}
+              onChange={(e) => setSearchText(e.target.value as string)}
+              startAdornment={
                 <InputAdornment position="start">
                   <SearchRounded />
                 </InputAdornment>
-              ),
-            }}
-          />
+              }
+              endAdornment={
+                searchText.length ? (
+                  <InputAdornment position="end">
+                    <IconButton onClick={clearSearch}>
+                      <Close />
+                    </IconButton>
+                  </InputAdornment>
+                ) : undefined
+              }
+            />
+          </FormControl>
         </Grid>
         {searchFoodItems.map((foodItem, idx) => (
-          <Grid key={idx}>
+          <Grid key={idx} xs={8} sm={7} md={6} lg={5} xl={4}>
             <FoodItemCard foodItem={foodItem} />
           </Grid>
         ))}
