d93dfe92acf78135cd725f9fd579983057428109
Omar Zeghouani <omarzeghouanii@gmail.com>
2023-03-26 18:31:37 +0100

    fix: only initialise with credentials file if exists

--- internal/app/firebase/app.go
+++ internal/app/firebase/app.go
@@ -13,15 +13,22 @@ type FirebaseApp struct {
 }
 
 func NewApp(cfg Config) (*FirebaseApp, error) {
-	var opt option.ClientOption
 	if cfg.Credentials != "" {
-		opt = option.WithCredentialsFile(cfg.Credentials)
+		opt := option.WithCredentialsFile(cfg.Credentials)
+		app, err := firebase.NewApp(context.Background(), nil, opt)
+		if err != nil {
+			return nil, fmt.Errorf("error initializing app with credentials file: %v", err)
+		}
+		return &FirebaseApp{
+			app: app,
+		}, nil
+	} else {
+		app, err := firebase.NewApp(context.Background(), nil)
+		if err != nil {
+			return nil, fmt.Errorf("error initializing app: %v", err)
+		}
+		return &FirebaseApp{
+			app: app,
+		}, nil
 	}
-	app, err := firebase.NewApp(context.Background(), nil, opt)
-	if err != nil {
-		return nil, fmt.Errorf("error initializing app: %v", err)
-	}
-	return &FirebaseApp{
-		app: app,
-	}, nil
 }
