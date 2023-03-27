8791650f991f346b7448c51b6df39eed4eb98eb2
Omar Zeghouani <59267627+ram02z@users.noreply.github.com>
2023-03-28 00:51:03 +0100

    fix: ensure that client is navigated to auth on error (#116)

--- ui/src/pages/Error/index.ts
+++
@@ -1,3 +0,0 @@
-import ErrorPage from './Error';
-
-export default ErrorPage;
--- ui/src/routes/Pages/Pages.tsx
+++ ui/src/routes/Pages/Pages.tsx
@@ -1,4 +1,3 @@
-import { ErrorBoundary } from 'react-error-boundary';
 import { Route, Routes } from 'react-router-dom';
 
 import Box from '@mui/material/Box';
@@ -6,7 +5,6 @@ import Box from '@mui/material/Box';
 import PrivateRoute from '@/components/PrivateRoute';
 import { NAVIGATION_DRAWER_WIDTH } from '@/config';
 import useLayout from '@/hooks/useLayout';
-import ErrorPage from '@/pages/Error';
 import routes from '@/routes';
 
 import { getPageHeight } from './utils';
@@ -18,47 +16,43 @@ function Pages() {
 
   return (
     <Box sx={{ height: (theme) => getPageHeight(theme), p: 3, ml: marginLeft }}>
-      <ErrorBoundary FallbackComponent={ErrorPage}>
-        <Routes>
-          {Object.values(routes).map(
-            ({ path, component: Component, subComponents, requireAuth }) => {
-              return (
-                <Route
-                  key={path}
-                  path={path}
-                  element={
-                    requireAuth ? (
-                      <PrivateRoute>
-                        <Component />
-                      </PrivateRoute>
-                    ) : (
-                      <Component />
-                    )
-                  }
-                >
-                  {subComponents.map(({ path, component: SubComponent }) => {
-                    return (
-                      <Route
-                        key={path}
-                        path={path}
-                        element={
-                          requireAuth ? (
-                            <PrivateRoute>
-                              <SubComponent />
-                            </PrivateRoute>
-                          ) : (
-                            <SubComponent />
-                          )
-                        }
-                      />
-                    );
-                  })}
-                </Route>
-              );
-            },
-          )}
-        </Routes>
-      </ErrorBoundary>
+      <Routes>
+        {Object.values(routes).map(({ path, component: Component, subComponents, requireAuth }) => {
+          return (
+            <Route
+              key={path}
+              path={path}
+              element={
+                requireAuth ? (
+                  <PrivateRoute>
+                    <Component />
+                  </PrivateRoute>
+                ) : (
+                  <Component />
+                )
+              }
+            >
+              {subComponents.map(({ path, component: SubComponent }) => {
+                return (
+                  <Route
+                    key={path}
+                    path={path}
+                    element={
+                      requireAuth ? (
+                        <PrivateRoute>
+                          <SubComponent />
+                        </PrivateRoute>
+                      ) : (
+                        <SubComponent />
+                      )
+                    }
+                  />
+                );
+              })}
+            </Route>
+          );
+        })}
+      </Routes>
     </Box>
   );
 }
