914072074822b662998ced88fb946443039311dc
Omar Zeghouani <omarzeghouanii@gmail.com>
2023-03-28 00:42:43 +0100

    remove errorpage content

--- ui/src/pages/Error/Error.tsx
+++ ui/src/pages/Error/Error.tsx
@@ -1,14 +1,9 @@
 import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-
-import { Button, Stack, Typography } from '@mui/material';
 
 import { Code, ConnectError } from '@bufbuild/connect-web';
 import { useSnackbar } from 'notistack';
 
 import { useSignOut } from '@/hooks/useSignOut';
-import routes from '@/routes';
-import { Pages } from '@/routes/types';
 
 type ErrorProps = {
   error: Error;
@@ -25,14 +20,8 @@ function ErrorPage({ error }: ErrorProps) {
   } else {
     enqueueSnackbar('Something went wrong. Please try again later.', { variant: 'error' });
   }
-  return (
-    <Stack spacing={2} justifyContent="center" alignItems="center" height="100%">
-      <Typography>An error occured</Typography>
-      <Button component={Link} to={routes[Pages.Auth].path}>
-        Reauthenticate
-      </Button>
-    </Stack>
-  );
+
+  return <></>;
 }
 
 export default ErrorPage;
