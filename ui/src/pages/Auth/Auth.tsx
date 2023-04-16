70f8a40365acf5c9b7686a357cdd30e859f2c980
Omar Zeghouani <59267627+ram02z@users.noreply.github.com>
2023-04-16 19:50:11 +0100

    feat: add custom icon as favicon and logo (#125)

--- ui/src/pages/Auth/Auth.tsx
+++ ui/src/pages/Auth/Auth.tsx
@@ -1,7 +1,8 @@
 import { Link, Navigate, useOutlet } from 'react-router-dom';
 
-import { Button, Stack, Typography } from '@mui/material';
+import { Button, Stack, Typography, useTheme } from '@mui/material';
 
+import { ReactComponent as Logo } from '@/assets/logo.svg';
 import { useCurrentUser } from '@/hooks/useCurrentUser';
 import routes from '@/routes';
 import { Pages } from '@/routes/types';
@@ -9,11 +10,13 @@ import { Pages } from '@/routes/types';
 function Auth() {
   const outlet = useOutlet();
   const user = useCurrentUser();
+  const theme = useTheme();
   if (user !== null) return <Navigate to={routes[Pages.Account].path} />;
   if (outlet) return outlet;
 
   return (
     <Stack direction="column" justifyContent="center" alignItems="center" height="100%" spacing={2}>
+      <Logo height={41} width={41} fill={theme.palette.primary.dark} />
       <Typography>Welcome to Neutral Diet</Typography>
       <Typography>Log in with your account to continue</Typography>
       <Stack direction="row" justifyContent="center" alignItems="center" spacing={2}>
